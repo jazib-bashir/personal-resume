fcaf7ba5c664f737b7b05ddc5399f9853556a71d
Jazib Bashir <jazibbashir@gmail.com>
2020-03-07 14:28:44 +0500

    ui_navbar - implemented sticky navbar

--- src/App.js
+++ src/App.js
@@ -5,13 +5,16 @@ import Profile from './components/profile/profile';
 import Hobies from './components/profile/hobies';
 import Resume from './components/resume/resume';
 
+import Sticky from 'react-sticky-el';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Home />
-      <Navigation />
+      <Sticky style={{backgroundColor: '#FFFFFF', zIndex: '999'}}>
+        <Navigation />
+      </Sticky>
       <Profile />
       <Hobies />
       <Resume />
